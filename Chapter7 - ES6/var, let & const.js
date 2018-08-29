c45a1db9955511f6705d3b124fe9d125799e242e
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-29 10:59:56 +0700

    Chapter7 - var, let & const
    When should be use var, let or const ?

--- Chapter7 - ES6/var, let & const.js
+++ Chapter7 - ES6/var, let & const.js
@@ -1,33 +1,57 @@
-var id = 123;
+// var id = 123;
 
-// có thể khai báo trồng id ở trên
-var id = '123';
+// // có thể khai báo trồng id ở trên
+// var id = '123';
+
+// if (true) {
+//     var i = 2;
+// }
+
+// function sayHi() {
+//     var hi = 'hi';
+// }
+
+// console.log(i);
+// // console.log(hi); // không thể gọi trực tiếp hi thì scope của nó chỉ ở trong function
+
+
+// // // có thể thay đổi giá trị ở dưới
+// // let address = 'HCM';
+// // address = 'HN';
+
+// // // không thể thay đổi giá trị ở dưới
+// // const street = 35;
+// // street = 45;
+
+
+// // đổi với object có thể thay đổi value bên trong nó, nhưng khi thay đổi chính nó thì không được
+// const user = {
+//     firstname: 'Nguyen'
+// }
+
+// user.firstname = 'Huy';
+// user = {};
+
+
+
+
+// khi nào thì sử dụng var, let & const
+
+var arrays = [];
+
+for (let i = 0; i < 5; i++) {
+    arrays[i] = function () {
+        console.log(i);
+    }
+}
 
 if (true) {
-    var i = 2;
+    // let or const in order to create variable
 }
 
-function sayHi() {
-    var hi = 'hi';
-}
+arrays[0]();
+arrays[1]();
+arrays[2]();
+arrays[3]();
+arrays[4]();
 
-console.log(i);
-// console.log(hi); // không thể gọi trực tiếp hi thì scope của nó chỉ ở trong function
-
-
-// // có thể thay đổi giá trị ở dưới
-// let address = 'HCM';
-// address = 'HN';
-
-// // không thể thay đổi giá trị ở dưới
-// const street = 35;
-// street = 45;
-
-
-// đổi với object có thể thay đổi value bên trong nó, nhưng khi thay đổi chính nó thì không được
-const user = {
-    firstname: 'Nguyen'
-}
-
-user.firstname = 'Huy';
-user = {};
