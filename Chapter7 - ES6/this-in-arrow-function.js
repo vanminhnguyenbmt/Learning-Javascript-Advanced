c21de7a7d6cc87ad8c37e61e64982f08e427c433
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-08-30 09:36:31 +0700

    Chapter7 - When should be avoid arrow function
    ES6

--- Chapter7 - ES6/this-in-arrow-function.js
+++ Chapter7 - ES6/this-in-arrow-function.js
@@ -1,27 +1,45 @@
 // This in arrow function
 
-var hi = function () {
+// var hi = function () {
+//     console.log(this);
+// }
+
+// var sayHello = () => {
+//     console.log(this);
+// }
+
+// // hi();
+// // sayHello();
+
+// var firstname = 'Huy';
+
+// var user = {
+//     firstname: 'Nguyen',
+//     showName: function() {
+//         console.log(this.firstname);
+//     },
+//     logName: () => { // không nên dùng arrow function ở trong objects
+//         console.log(this.firstname);
+//     }
+// }
+
+// user.showName();
+// user.logName();
+
+function User(firstname) {
+    this.firstname = 'Nguyen';
+}
+
+// User.prototype.showName = () => { // dùng arrow function thì this đang là global object
+//     console.log(this);
+//     console.log(this.firstname);
+// }
+
+User.prototype.showName = function() {
     console.log(this);
+    console.log(this.firstname);
 }
 
-var sayHello = () => {
-    console.log(this);
-}
+var nguyen = new User();
 
-// hi();
-// sayHello();
-
-var firstname = 'Huy';
-
-var user = {
-    firstname: 'Nguyen',
-    showName: function() {
-        console.log(this.firstname);
-    },
-    logName: () => {
-        console.log(this.firstname);
-    }
-}
-
-user.showName();
-user.logName();
+nguyen.showName();
