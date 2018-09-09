94e3a840a145f2b3956c9fdcf9b577b78c45bb41
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-09-09 22:06:23 +0700

    Chapter12 - Use strict
    Mind

---
+++ Chapter12 - Mind/use-strict.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var hi = {
+    
+}
+
+// delete hi;
+
+// var age = 22;
+
+// console.log(age);
+
+// eval('var age = 22');
+// console.log(age);
+
+// function sayHi() {
+//     "use strict";
+//     age = 16;
+//     console.log(age);
+// }
+
+// sayHi();
